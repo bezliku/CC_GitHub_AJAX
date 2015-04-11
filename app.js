752dcfc76227fd402d6fb8414b5b493c430c1bc1
Artur Delura <adelura@gmail.com>
2015-04-11 11:22:34 +0200

    Added stuff required by heroku.

---
+++ app.js
@@ -0,0 +1,11 @@
+var jsonServer = require("json-server");
+var port = 80;
+
+var router = jsonServer.router("db.json");
+var server = jsonServer.create();
+
+server.use(router);
+server.listen(port);
+
+console.log('Server listening on port:', port);
+console.log('Open: http://127.0.0.1:' + port);
