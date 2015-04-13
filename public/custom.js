6d86d2f6d6b152f0359aef200cce9cbaf00fec2d
bezliku <emhaw@o2.pl>
2015-04-13 16:03:16 +0200

    fixed#2, fixed#4, fixed#5, fixed#6, fixed#7

--- public/custom.js
+++ public/custom.js
@@ -1,31 +1,35 @@
 /*Utworzenie posta i nadanie elementom treści, oraz odpowiednich klas i id*/
 
 function createPost(data) {
-    var s = document.createElement("section"),
-        a = document.createElement("article"),
-        id = document.createElement("h1"),
-        h2 = document.createElement("h2"),
-        h3 = document.createElement("h3"),
-        p = document.createElement("p"),
-        b = document.createElement("div");
+  var s = document.createElement("section"),
+      a = document.createElement("article"),
+      id = document.createElement("h1"),
+      h2 = document.createElement("h2"),
+      h3 = document.createElement("h3"),
+      p = document.createElement("p"),
+      b = document.createElement("div");
     
-    id.innerText = data.id;
-    h2.innerText = data.title;
-    h3.innerText = "Autor: " + data.author;
-    p.innerText = data.content;
-    b.innerText = "Przeczytaj";
+  id.innerText = data.id;
+  h2.innerText = data.title;
+  h3.innerText = "Autor: " + data.author;
+  p.innerText = data.content;
+  b.innerText = "Przeczytaj";
 
-    p.setAttribute("class", "unactive");
-    b.setAttribute("class", "readMore"); 
-    id.setAttribute("id", data.id);
+  p.setAttribute("class", "unactive");
+  b.setAttribute("class", "readMore"); 
+  id.setAttribute("id", data.id);
 
-    document.body.appendChild(s);
-    s.appendChild(id);
-    s.appendChild(a);
-    a.appendChild(h2);
-    a.appendChild(h3);
-    a.appendChild(b);
-    a.appendChild(p);
+  document.body.appendChild(s);
+  s.appendChild(id);
+  s.appendChild(a);
+  a.appendChild(h2);
+  a.appendChild(h3);
+  a.appendChild(b);
+  a.appendChild(p);
+
+  $(".readMore").click(function(){
+    $(this).next("p").toggleClass("unactive active");
+  });
 };
 
 /*Dodanie wszystkich postów.
@@ -36,39 +40,41 @@ function success(data) {
   for (var i = 0; i < data.length; i++) {
     var post = data[i];
     var postElem = createPost(post);
-    
   };
-
-  $(".readMore").click(function(){
-      $(this).next("p").toggleClass("active");
-  });
-
 };
-
 
 $.ajax({
   url: "/posts",
   success: success
 });
 
-
 /*Pokazanie formularzy po kliku w buttony*/
 $("#newPost").click(function(){
-  $("#newPostForm").slideDown("500");
+  $("#newPostFormContainer").slideDown("500");
+  $("#tytul").focus();
 });
 
 $("#deletePost").click(function(){
-  $("#deletePostForm").slideDown("500");
+  $("#deletePostFormContainer").slideDown("500");
+  $("#id").focus();
+});
+
+/*Schowanie formularzy po kliku w krzyżyk lub klawisz ESC*/
+$(".closeForm").click(function(){
+  $("#newPostFormContainer").hide();
+  $("#deletePostFormContainer").hide();
+});
+
+$("body").keyup(function(e){
+  if (e.keyCode == 27) $('.closeForm').click();
 });
 
 /*Schowanie formularza po kliku "Wyślij"
   Zapisanie dodanego postu
 */
 
-
 $('#sendPost').click(function(){
-
-  $("#newPostForm").hide();
+  $("#newPostFormContainer").hide();
 
   var formularz = $("#newPostForm");
   var data = formularz.serialize();
@@ -80,6 +86,15 @@ $('#sendPost').click(function(){
       success: createPost
     });
 
+  $("#newPostForm").each(function(){
+    this.reset();
+  });
+});
+
+$('#newPostForm').each(function() {
+  $(this).find('input').keypress(function(e) {
+    if(e.keyCode == 13) $('#sendPost').click();
+  });
 });
 
 /*Schowanie formularza po kliku w "Usuń"
@@ -89,29 +104,32 @@ $('#sendPost').click(function(){
 
 $('#deletePostID').click(function(){
 
-  $("#deletePostForm").hide();
+  $("#deletePostFormContainer").hide();
 
   var formularz = $("#deletePostForm").serializeArray();
   var data = formularz[0].value;
   
   $.ajax({
-      url: "/posts/" + data,
-      method: "DELETE",
-      data: data,
-      success: function() {
-
-  var data = formularz[0].value;
-  var idElement = document.getElementById(data);
-  var parent = idElement.parentElement;
-  parent.remove();
-
+    url: "/posts/" + data,
+    method: "DELETE",
+    data: data,
+    success: function() {
+      var data = formularz[0].value;
+      var idElement = document.getElementById(data);
+      var parent = idElement.parentElement;
+      parent.remove();
     }
-
   });
 
+  $("#id").val("");
 });
 
 
-
-
-
+$('#deletePostForm').each(function() {
+  $(this).find('input').keypress(function(e) {
+    if(e.keyCode == 13) {
+      event.preventDefault();
+      $('#deletePostID').click();
+    }
+  });
+});
